61f33c1fe423953e0fdbbe696be23f048f8048b8
JOÃO PEDRO CREPALDI GONÇALVES <joao.goncalves231@etec.sp.gov.br>
2025-06-05 17:44:32 -0300

    storage

--- src/screens/Storage.js
+++ src/screens/Storage.js
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
 
 const [emailVal, setEmail] = useState('');
 const [senhaVal, setSenha] = useState('');
@@ -6,23 +7,32 @@ const [senhaVal, setSenha] = useState('');
 //função para salvar o email
 const saveEmail = async (e) => {
     try {
-        await AsyncStorage.setItem('emailVal', JSON.stringify(e));
-    } catch(error){
-        console.log('Erro ao salvar o email', error);
+        await AsyncStorage.setItem('emailVal', JSON.stringify(e));//na função "salvarEmail", é utilizada o método setItem(pegando o valor de email) e transforma em string
+    } catch (error) {
+        console.log('Erro ao salvar o email', error);//se não der certo, é pegado um erro e printado
     }
 };
 
 //função para salvar a senha
 const saveSenha = async (e) => {
     try {
-        await AsyncStorage.setItem('senhaVal', JSON.stringify(e));
-    } catch(error){
+        await AsyncStorage.setItem('senhaVal', JSON.stringify(e));//é feito a mesma coisa para o senha
+    } catch (error) {
         console.log('Erro ao salvar o senha', error);
     }
 };
 
-const loadHome = async () => {
-    if(saveEmail !== null && saveSenha !== null){
+useEffect(() => {// detector de eventos
+    const loadHome = async () => {
+        const storedEmail = await AsyncStorage.getItem('emailVal');//verificando o valor salvo
+        const storedSenha = await AsyncStorage.getItem('senhaVal');
 
-    }
-}
+        if (saveEmail !== null && saveSenha !== null) {
+            navigation.navigate('Home');
+        } else {
+            navigation.navigate('Login');
+        }
+    };
+
+    loadHome();
+}[navigation]);
